e4b3e04158635bcc7738817d243f9b1c2863557f
NaLette Brodnax <nmbrodnax@gmail.com>
2017-08-07 18:32:16 -0400

    queries by site, search type, and tag

--- stackexchange.py
+++ stackexchange.py
@@ -12,19 +12,22 @@ def main():
 
     sites = ['stackoverflow', 'stats', 'datascience']
     queries = ['zelig', 'weights', 'bootstrap', 'hierarchical', 'multilevel',
-               'time series', 'visualization', 'plot', 'database']
+               'time series', 'database']
     tags = ['r', 'r-zelig', 'statistics']
     
-    # request data from stackexchange api 
-    response = get_questions('2017-01-01', '2017-08-06', 'stackoverflow',
-                         search = 'zelig', search_type = 'query')
-    data = response.json()
-    print("StackExchange API quota remaining: " + str(data['quota_remaining']))
-    questions = data['items']  # list of dictionaries
-    
+    # request data from stackexchange api
+    questions = []
+    for site in sites:
+        for query in queries:
+            q = get_questions('2016-08-06', '2017-08-06', site = site,
+                              search = query + "&tagged=r", search_type = 'query',
+                              page = 1, pagesize = 100)
+            questions = [*questions, *q]
+
     # save results to csv file
     fieldnames = ['tags', 'view_count', 'answer_count', 'last_activity_date',
-                  'creation_date', 'link', 'title', 'site', 'query']
+                  'creation_date', 'link', 'title', 'site', 'search',
+                  'search_type']
 
     with open("stackexchange.csv", "w") as outfile:
         writer = csv.DictWriter(outfile, fieldnames)
@@ -36,23 +39,43 @@ def main():
             writer.writerow(tmp)
 
 
-def get_questions(from_date, to_date, site, search, search_type = 'query'):
+def get_questions(from_date, to_date, site, search, search_type = 'query',
+                  page = 1, pagesize = 100):
     """Returns the results of an http GET request to the StackExchange API v2.2.
     Accepts dates in format YYYY-MM-DD.  Accepts search_type of 'query' to
     search question titles or 'tag' to search question tags.
-    str -> Response object from requests library"""
+    str, int -> Response object from requests library"""
     
-    url = "https://api.stackexchange.com/2.2/search?" + "fromdate=" + \
-          from_date + "&todate=" + to_date + "&order=desc&sort=votes&site=" + \
-          site
-    
-    if search_type == 'query':
-        return requests.get(url + "&intitle=" + search)
-    elif search_type == 'tag':
-        return requests.get(url + "&tagged=" + search)
-    else:
-        print("Invalid search_type value ('query' or 'tag' only).")
+    host = "https://api.stackexchange.com/2.2/search?"
+    options = "&pagesize=" + str(pagesize) + "&fromdate=" + from_date + "&todate=" + to_date + \
+          "&order=desc&sort=votes&site=" + site
+    has_more = True 
+    questions = []
 
+    while has_more:
+        if search_type not in ('query', 'tag'):
+            has_more = False
+            print("Invalid search_type value ('query' or 'tag' only).")
+        elif search_type == 'query':
+            url = host + "page=" + str(page) + options + "&intitle=" + search
+        else:
+            url = host + "page=" + str(page) + options + "&tagged=" + search
+        response = requests.get(url)
+        # print("API response: " + str(response))
+        
+        data = response.json()
+        
+        for dict_i in data['items']:
+            dict_i['site'] = site
+            dict_i['search'] = search
+            dict_i['search_type'] = search_type
+            questions.append(dict_i)
+        has_more = data['has_more']
+        page += 1
+    print(str(len(questions)) + " questions returned.")
+    print("API quota remaining: " + str(data['quota_remaining']))
+    return questions
+    
 
 if __name__ == '__main__':
     main()
