7cffdf5aa63b6e6f250672ad9b85ba3ef20666a1
NaLette Brodnax <nmbrodnax@gmail.com>
2017-08-14 19:17:21 -0400

    create csv file by tag

--- reshape_by_tag.py
+++ reshape_by_tag.py
@@ -4,21 +4,44 @@ import csv
 
 
 def main():
-    fieldnames = ['tags', 'view_count', 'answer_count', 'last_activity_date',
-                  'creation_date', 'link', 'title', 'site', 'question_id',
-                  'topic', 'search_type']
+    srcfieldnames = ['tags', 'view_count', 'answer_count', 'last_activity_date',
+                     'creation_date', 'link', 'title', 'site', 'question_id',
+                     'topic', 'search_type']
     with open('by_topic.csv', 'r') as sourcefile:
-        reader = csv.DictReader(sourcefile, fieldnames)
+        reader = csv.DictReader(sourcefile, srcfieldnames)
         next(reader, None)
-        all_tags = []
-        all_questions = []
+        questions = []
         for i in reader:
-            [all_tags.append(t) for t in i.get('tags') if t not in all_tags]
-            [all_questions.append(q) for q in [i.get('question_id')] if q not in
-             all_questions]
+            questions.append(i)
+    print(len(questions))
+    print(questions[0]['tags'])
+    mystr = questions[0]['tags']
+    print(type(mystr[2:-2].split("', '")))
 
-    print('Tags: ' + str(len(all_tags)))
-    print('Questions: ' + str(len(all_questions)))
+    outfieldnames = ['tag', 'question_count', 'view_count', 'answer_count']
+    with open('by_tag.csv', 'w') as outfile:
+        writer = csv.DictWriter(outfile, outfieldnames)
+        writer.writeheader()
+        question_ids = []
+        tag_names = []
+        tags = {}
+        for q in questions:
+            if q.get('question_id') not in question_ids:
+                question_ids.append(q.get('question_id'))
+                qtags = q['tags'][2:-2].split("', '")
+                for t in qtags:
+                    if t not in tag_names:
+                        tag_names.append(t)
+                        tags[t] = {'tag': t, 'question_count': 1,
+                                   'view_count': int(q['view_count']),
+                                   'answer_count': int(q['answer_count'])}
+                    else:
+                        tags[t]['question_count'] += 1
+                        tags[t]['view_count'] += int(q['view_count'])
+                        tags[t]['answer_count'] += int(q['answer_count'])
+        for tag in tags:
+            writer.writerow(tags.get(tag))
+    print(tag_names)
 
 
 if __name__ == '__main__':
